3ad872641c95679b65b4bc1a17bc49f41a9c6b5e
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-01-07 13:11:06 +0000

    Update job scripts

--- jobs/run.sh
+++ jobs/run.sh
@@ -2,20 +2,26 @@
 
 export CONTEST=$(echo "$JOB_NAME")  #  | cut -c7-20)
 projectdir=/data/project/ukbot
-logfile=${projectdir}/logs/${CONTEST}.upload.log
+logfile=${projectdir}/logs/${CONTEST}.run.log
 statusfile=${projectdir}/logs/${CONTEST}.status
 
 echo "-----------------------------------------------------------------"
 cd ${projectdir}
-echo "$(date) : Starting '$CONTEST' job ($JOB_ID) on $HOSTNAME" | tee $logfile
+echo "$(date) : Starting '$CONTEST' job ($JOB_ID) on $HOSTNAME." | tee $logfile
 START=$(date +%s)
+echo "running $START" >| $statusfile
+
+# Start mem logger
+./mem_logger.sh &
 
 . ${projectdir}/ENV/bin/activate
-cd ${projectdir}/bot
 
+cd ${projectdir}/bot
 # set -o pipefail  # doesn't work when run through the task schedueler
-python uploadplot.py --contest "${CONTEST}" 2>&1 | tee -a $logfile
+python ukbot.py --contest "${CONTEST}" --action "uploadplot" "$@" 2>&1 | tee -a $logfile
 status="${PIPESTATUS[0]}"
 
 echo "$(date) : Job $JOB_NAME ($JOB_ID) on $HOSTNAME finished with exit code $status" | tee -a $logfile
-
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "$status $DIFF $(date)" >| $statusfile

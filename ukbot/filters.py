6f45fbb009bf8113cbc6d4e8dfe2f094263bd005
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-04-09 14:07:08 +0000

    Add automatic retry for sparql queries

--- ukbot/filters.py
+++ ukbot/filters.py
@@ -9,12 +9,34 @@ import json
 import time
 import urllib
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 from mwtemplates.templateeditor2 import TemplateParseError
 
 from .common import _, InvalidContestPage
 from .site import WildcardPage
 
 logger = logging.getLogger(__name__)
+
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.8,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 
 class CategoryLoopError(Exception):
@@ -684,7 +706,7 @@ class SparqlFilter(Filter):
         self.fetch()
 
     def do_query(self, querystring):
-        response = requests.get(
+        response = requests_retry_session().get(
             'https://query.wikidata.org/sparql',
             params={
                 'query': querystring,

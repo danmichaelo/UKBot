1b8bafddcfe368616e0e85aa3bb6e48fba28f279
Dan Michael O. Heggø <danmichaelo@gmail.com>
2022-06-05 13:35:28 +0200

    Fix backwards Python compat

--- ukbot/contributions.py
+++ ukbot/contributions.py
@@ -283,7 +283,7 @@ class UserContributions(object):
 
         if article.name in self.labels:
             formatted = '[[%s|%s]]' % (article.link(), self.labels[article.name])
-        elif article.link() == f":{article.name}":
+        elif article.link() == ":" + article.name:
             formatted = '[[%s]]' % article.name
         else:
             formatted = '[[%s|%s]]' % (article.link(), article.name)
